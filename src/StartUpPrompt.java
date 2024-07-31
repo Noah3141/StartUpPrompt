d26e52eb183de42c8c5ada17aaa57549ddbe962f
app-core <Noah3141@gmail.com>
2024-07-31 16:56:07 +0000

    ws

--- src/StartUpPrompt.java
+++ src/StartUpPrompt.java
@@ -1,13 +1,12 @@
 public class StartUpPrompt {
 
-
 	public static void main(String[] args) {
 		System.out.println("Welcome to App Core");
-	
+		
 		File file = new File();
-
+		
 		System.out.println(file.open());
 	}
-
+	
 }
 
